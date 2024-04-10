a061bd23697248299a7a2921b760c93ea173717d
Hans-Peter Suter <hps@treetron.ch>
2024-04-10 12:01:59 +0200

    unsucessful requests raise an error

--- tests/test_api_client.py
+++ tests/test_api_client.py
@@ -1,4 +1,5 @@
 import json
+import pytest
 from cashctrl_api import CashCtrlAPIClient
 
 def test_person_list():
@@ -6,6 +7,7 @@ def test_person_list():
     response = cc.get("person/list.json")
 
 def test_person_flatten_dict():
+    # create
     contact = {
         "firstName": "Tina",
         "lastName": "Test",
@@ -17,9 +19,17 @@ def test_person_flatten_dict():
             }],
         "titleId": 2
     }
-
     cc = CashCtrlAPIClient()
     response = cc.post("person/create.json", data=contact)
     id = response["insertId"]
+    # read
     response = cc.get("person/read.json", params={'id': id})
+    # delete
     response = cc.post("person/delete.json", params={'ids': id})
+
+def test_non_success_and_msg():
+    cc = CashCtrlAPIClient()
+    with pytest.raises(Exception) as e:
+        assert(cc.post("person/create.json"))
+    assert str(e.value) == 'API call failed with message: Either first name, last name or company must be set.'
+  
