89d327a13f24563526afbfd733181fe24ab98a8a
Hans-Peter Suter <hps@treetron.ch>
2024-04-12 22:13:59 +0200

    disable mirror_files test (github action issue)

--- tests/test_mirror_files.py
+++ tests/test_mirror_files.py
@@ -3,11 +3,11 @@ from cashctrl_api import CashCtrlAPIClient
 
 def test_mirror_files():
 
-    if True:
+    if False:
         # I'm not sure if we can/should run this code on every commit.
         # It will test run one time and then be disabled again (atm)
 
-        # In addition it doesn't work b/c the absolute path is wrong,
+        # In addition it doesn't work within github actions b/c the absolute path is wrong,
         # maybe because of [this](https://stackoverflow.com/questions/67586339/github-actions-get-absolute-path-to-working-directory)?
 
         cc = CashCtrlAPIClient()
