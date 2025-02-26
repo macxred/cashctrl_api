47308e5d7953d3585de329ee2bea4fd7b5b3f07c
Alex the wizard <sashok1111122222@gmail.com>
2025-02-26 15:39:53 +0200

    feat: update test suit for profit center entity

--- tests/test_profit_centers.py
+++ tests/test_profit_centers.py
@@ -1,14 +1,13 @@
-"""Unit tests for cached profit centers."""
+"""Unit tests for profit centers."""
 
-import time
-from cashctrl_api import CachedCashCtrlClient, CashCtrlClient
+from cashctrl_api import CashCtrlClient
 import pandas as pd
 import pytest
 
 
 @pytest.fixture(scope="module")
-def cc_client() -> CachedCashCtrlClient:
-    return CachedCashCtrlClient()
+def cc_client() -> CashCtrlClient:
+    return CashCtrlClient()
 
 
 @pytest.fixture(scope="module")
@@ -29,29 +28,8 @@ def profit_centers(cc_client):
         cc_client.post("account/costcenter/delete.json", params={"ids": ids})
 
 
-def test_profit_centers_cache_is_none_on_init(cc_client):
-    assert cc_client._profit_centers_cache is None
-    assert cc_client._profit_centers_cache_time is None
-
-
 def test_cached_profit_centers_same_to_actual(cc_client, profit_centers):
     pd.testing.assert_frame_equal(cc_client.list_profit_centers(), profit_centers)
-
-
-def test_profit_center_cache_timeout():
-    cc_client = CachedCashCtrlClient(cache_timeout=1)
-    cc_client.list_profit_centers()
-    assert not cc_client._is_expired(cc_client._profit_centers_cache_time)
-    time.sleep(1)
-    assert cc_client._is_expired(cc_client._profit_centers_cache_time)
-
-
-def test_profit_center_cache_invalidation():
-    cc_client = CachedCashCtrlClient()
-    cc_client.list_profit_centers()
-    assert not cc_client._is_expired(cc_client._profit_centers_cache_time)
-    cc_client.invalidate_profit_centers_cache()
-    assert cc_client._is_expired(cc_client._profit_centers_cache_time)
 
 
 def test_profit_center_from_id(cc_client, profit_centers):
