350b528a630d4f6138bd92e6ed205862690b1a48
Alex the wizard <sashok1111122222@gmail.com>
2025-03-11 20:17:35 +0200

    refactor: remove useless fixture and test

--- tests/test_fiscal_periods.py
+++ tests/test_fiscal_periods.py
@@ -1,7 +1,6 @@
 """Unit tests for fiscal periods."""
 
 from cashctrl_api import CashCtrlClient
-import pandas as pd
 import pytest
 
 
@@ -10,21 +9,12 @@ def cc_client() -> CashCtrlClient:
     return CashCtrlClient()
 
 
-@pytest.fixture(scope="module")
-def fiscal_periods(cc_client):
-    # Explicitly call the base class method to circumvent the cache.
-    return CashCtrlClient.list_fiscal_periods(cc_client)
-
-
-def test_cached_fiscal_periods_same_to_actual(cc_client, fiscal_periods):
-    pd.testing.assert_frame_equal(cc_client.list_fiscal_periods(), fiscal_periods)
-
-
-def test_fiscal_period_from_id(cc_client, fiscal_periods):
+def test_fiscal_period_from_id(cc_client):
+    fiscal_periods = cc_client.list_fiscal_periods()
     assert (
         cc_client.fiscal_period_from_id(fiscal_periods["id"].iat[0])
         == fiscal_periods["name"].iat[0]
-    ), "Cached fiscal period name doesn't correspond to actual name"
+    ), "Mapped fiscal period name doesn't correspond to actual name"
 
 
 def test_fiscal_period_from_id_invalid_id_raises_error(cc_client):
@@ -36,11 +26,12 @@ def test_fiscal_period_from_id_invalid_id_returns_none_with_allowed_missing(cc_c
     assert cc_client.fiscal_period_from_id(99999999, allow_missing=True) is None
 
 
-def test_fiscal_period_to_id(cc_client, fiscal_periods):
+def test_fiscal_period_to_id(cc_client):
+    fiscal_periods = cc_client.list_fiscal_periods()
     assert (
         cc_client.fiscal_period_to_id(fiscal_periods["name"].iat[0])
         == fiscal_periods["id"].iat[0]
-    ), "Cached fiscal period id doesn't correspond to actual id"
+    ), "Mapped fiscal period id doesn't correspond to actual id"
 
 
 def test_fiscal_period_to_id_with_invalid_fiscal_period_raises_error(cc_client):
